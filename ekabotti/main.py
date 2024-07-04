9591b8f8a22750e958495d6229a08ba1787b36cd
Matti Österbacka <matti.osterbacka@gmail.com>
2024-07-04 20:33:31 +0300

    trying messages for saving statistics

--- ekabotti/main.py
+++ ekabotti/main.py
@@ -12,6 +12,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 intents.messages = True
+#intents.read_message_history = True
 
 client = discord.Client(intents=intents)
 
@@ -21,21 +22,34 @@ async def on_ready():
     print("a")
     #await channel.send("alku")
     print("b")
+    channel = await client.fetch_channel(posting_channel_id)
+    first_message = await fetch_first_message(False, channel)
+    print(first_message.content)
+    #clear_file()
+    await message_first_to_logging_channel(first_message)    
+    #write_first_into_file(first_message)
+    #await channel2.send(read_stats_from_file(first_message, channel2.members))
+    #print(read_stats_from_file(first_message, channel.members))
+    print(await get_statistics_from_logging_channel(first_message, channel.members))
+   # quit()
+
+async def fetch_first_message(test, channel):
     dt = datetime.combine(date=date.today(), time=time.min).astimezone(tz=timezone(timedelta(hours=2), 'Europe/Helsinki'))
-    print(dt)
-    channel2 = await client.fetch_channel(posting_channel_id)
+    if test:
+        dt = datetime.combine(date=date.fromisoformat("2024-06-18"), time=time.min).astimezone(tz=timezone(timedelta(hours=2), 'Europe/Helsinki'))
     first_message = None
     while first_message is None:
-        async for message in channel2.history(after=dt, limit=1):
+        async for message in channel.history(after=dt, limit=1):
             first_message = message
-    print(first_message.content)
-    #channel2.send(handle_file(first_message, channel2.members))
-    quit()
-    
-def handle_file(first_message, members):
+    return first_message
+        
+
+def write_first_into_file(first_message):
     f = open("ekat.txt", "a")
     f.write(str(first_message.author.id)+"\n")
     f.close()
+
+def read_stats_from_file(first_message, members):
     f = open("ekat.txt", "r")
     s = f.read().splitlines()
     f.close()
@@ -43,6 +57,10 @@ def handle_file(first_message, members):
     for member in members:
         r+= member.display_name + "|" + str(s.count(str(member.id))) + "\n"
     return r
+
+def clear_file():
+   f = open("ekat.txt", "w")
+   f.close()
 
 @client.event
 async def on_message(message):
@@ -52,5 +70,20 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         print(message.channel.id)
         await message.channel.send('Hello!')
+
+async def message_first_to_logging_channel(first_message):
+    channel = await client.fetch_channel(logging_channel_id)
+    await channel.send(first_message.author.id)
+
+async def get_statistics_from_logging_channel(first_message, members):
+    channel = await client.fetch_channel(logging_channel_id)
+    s = []
+    async for message in channel.history():
+        s.append(message.content)
+    r = "EKA oli " + first_message.author.display_name + "\n\n"
+    for member in members:
+        r+= member.display_name + "|" + str(s.count(str(member.id))) + "\n"
+    return r
+
 
 client.run(token)
